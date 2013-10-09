9674f174e3efa0339cd333f426f716f5d8085328
Aaron Fleshner <adfleshner@gmail.com>
2013-10-09 15:48:41 -0500

    Update GcmIntentService.java
    Oops, i added needed imports that i forgot

--- GCM Template/src/com/mattyork/pushtest/GcmIntentService.java
+++ GCM Template/src/com/mattyork/pushtest/GcmIntentService.java
@@ -34,7 +34,11 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v4.app.NotificationCompat;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
